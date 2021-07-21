81b37baa647826d77e5974f0c9896eb7614c743e
pbarretoo <76566240+pbarretoo@users.noreply.github.com>
2021-07-21 19:46:31 -0300

    Add files via upload
    Codigo para verificar Média, Moda e Mediana

---
+++ Exercicio_de_Estatistica.py
@@ -0,0 +1,7 @@
+lista_principal = [140, 300, 732, 395, 563, 118, 282, 430, 867, 910, 40, 823, 361,321, 778, 266, 381, 887]
+import statistics
+
+lista = statistics.mode(lista_principal)
+lista1 = statistics.median(lista_principal)
+lista2 = statistics.mean(lista_principal)
+print(lista,lista2,lista1)
